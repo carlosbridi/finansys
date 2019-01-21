e742d36b58332ceae2f5fd5e4db32d0239ffe4e4
Carlos Bridi <carlosbridi@gmail.com>
2019-01-21 00:16:33 -0200

    Create category model and service class

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,20 @@
+import { InMemoryDbService }  from "angular-in-memory-web-api"
+
+import {Category} from './pages/categories/shared/category.model'
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+    createDb(){
+        const categories: Category[] = [
+            {id: 1, name: "MOradia", description: "Pagamentos de conta da casa"},
+            {id: 2, name: "Saúde", description: "Gastos com saúde"},
+            {id: 3, name: "Salário", description: "Recebimento do salário"},
+            {id: 4, name: "Lazer", description: "Gastos diversos"},
+            {id: 5, name: "Freelas", description: "Trabalhos como freelancer"}
+        ];
+
+        return {categories}
+    }
+
+
+}
