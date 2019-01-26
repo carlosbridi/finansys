bdd4069100d00f89e378298098f1a65ba77e9589
Carlos Bridi <carlosbridi@gmail.com>
2019-01-26 08:22:23 -0200

    news improvement

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryFormComponent } from "./category-form/category-form.component";
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
